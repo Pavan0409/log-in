ef94c6eb54e0bc776e56eb9ba3b549f8d1a44977
Pavan0409 <105490073+Pavan0409@users.noreply.github.com>
2022-08-14 20:09:38 +0530

    forward refs

--- auth-context.js
+++ auth-context.js
@@ -1,28 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
   onLogout: () => {},
-  onLogin: (email, password) => {},
+  onLogin: (email, password) => {}
 });
 
 export const AuthContextProvider = (props) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
 
-    if (storedUserLoggedInInformation === "1") {
+    if (storedUserLoggedInInformation === '1') {
       setIsLoggedIn(true);
     }
   }, []);
 
   const logoutHandler = () => {
-    localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
+
   const loginHandler = () => {
-    localStorage.setItem("isLoggedIn", "1");
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
